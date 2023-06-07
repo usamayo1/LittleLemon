5ea796136096d1322ca05020845e9960815a9931
usamayo1 <@usamayo1>
2023-06-07 16:46:12 +0500

    Make Booking and Menu model

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Booking, Menu
 
 # Register your models here.
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'no_of_guests', 'bookingdate']
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'price', 'inventory']
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Booking(models.Model):
+    name = models.CharField(max_length=100)
+    no_of_guests = models.IntegerField()
+    bookingdate = models.DateTimeField(auto_now_add=True)
+    
+class Menu(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    inventory = models.IntegerField()
+    
+    def __str__(self):
+        return self.title
